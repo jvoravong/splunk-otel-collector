42f857d37bdd6c73814ca659f4ec2e9701e04082
jvoravong <jvoravong@splunk.com>
2025-04-11 09:37:29 -0600

    patch

--- .gitlab/update-downstream-chart.sh
+++ .gitlab/update-downstream-chart.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# NOTE: this script is meant to be run on the GitLab CI, it depends on GitLab CI variables
-# This script triggers a predefined set of GitHub Actions workflows in the signalfx/splunk-otel-collector
-# repository on the main branch. It uses the GitHub CLI (gh) to run each workflow sequentially and exits
-# early if any workflow fails to trigger.
+# NOTE: Triggers Github workflows to create PRs to update the downstream Helm chart.
+# - Repo: https://github.com/signalfx/splunk-otel-collector-chart
+# - Opens PRs to update instrumentation library dependencies.
+# - Opens a PR to prepare a new chart release after a collector release.
+# - This script uses the GitHub bot from this repo to trigger downstream chart PRs, ensuring GitHub
+#   checks/tests run automatically with these PRs. Without this, maintainers would have to manually
+#   reopen chart PRs to trigger the PR checks/tests. In the future, the chart repo may have its own
+#   GitLab mirror and Github bot integration.
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-OTEL_VERSION="${1:-latest}"
-BRANCH="${2:-create-pull-request/update-downstream-chart}"
-REPO="signalfx/splunk-otel-collector"
-REF="main"
-WORKFLOWS=(
-  "update_chart_dependencies.yaml"
-  "update_docker_images.yaml"
-  "release_drafter.yaml"
-)
 
 # shellcheck source-path=SCRIPTDIR
 source "${SCRIPT_DIR}/common.sh"
@@ -23,17 +18,27 @@ source "${SCRIPT_DIR}/common.sh"
 ROOT_DIR="${SCRIPT_DIR}/../"
 cd "${ROOT_DIR}"
 
+run_chart_github_workflows() {
+  local repo="signalfx/splunk-otel-collector-chart"
+  local repo_url="https://srv-gh-o11y-gdi:${GITHUB_TOKEN}@github.com/${repo}.git"
+  local branch="main"
+  local workflows=(
+    "update_chart_dependencies.yaml"
+    "update_docker_images.yaml"
+    "release_drafter.yaml"
+  )
+
+  for workflow in "${workflows[@]}"; do
+    echo "Triggering workflow: $workflow"
+    if gh workflow run "$workflow" --repo "$repo_url" --ref "$branch"; then
+      echo "Triggered: $workflow"
+    else
+      echo "Failed to trigger: $workflow — continuing..."
+    fi
+  done
+}
+
 setup_gpg
 import_gpg_secret_key "$GITHUB_BOT_GPG_KEY"
 setup_git
-
-for WORKFLOW in "${WORKFLOWS[@]}"; do
-  echo "Triggering workflow: $WORKFLOW"
-  if gh workflow run "$WORKFLOW" --repo "$REPO" --ref "$REF"; then
-    echo "Triggered: $WORKFLOW"
-  else
-    echo "Failed to trigger: $WORKFLOW — continuing..."
-  fi
-done
-
-echo "Workflow triggering complete (some may have failed)."
+run_chart_github_workflows
